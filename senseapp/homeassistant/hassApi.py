0046a3d10bb08c16ba76e2bd728b1dd0a1859d43
Dawid Podolak <dawidpod1@gmail.com>
2023-08-29 17:06:09 +0000

    Create base websocket communication with home assistant

--- senseapp/homeassistant/hassApi.py
+++ senseapp/homeassistant/hassApi.py
@@ -1,26 +1,53 @@
-import websocket
+# import websocket
 import json
+import websockets
+import logging
+import asyncio
+from homeassistant.auth_ha import auth
+from homeassistant.state import subscribe_to_state
+
+HOME_ASSISTANT_URL = "ws://172.30.32.1:8123/api/websocket"
+websocket = None
+MESSAGE_TYPE = "type"
 
 
-def on_message(ws, message):
-    print(f"Error: {message}")
-
-
-def on_error(ws, error):
-    print(f"Error: {error}")
-
-
-def on_close(ws, close_status_code, close_message):
-    print(f"Error: {error}")
-
-def on_open(ws):
-    print(f"Open socket")
-
-def start_websocket(url, token):
-    ws = websocket.WebSocketApp(
-        url=url,
-        on_message=on_message,
-        on_error=on_error,
-        on_close=on_close,
-        on_open=on_open
+async def start_websocket():
+    global websocket
+    logging.basicConfig(
+        format="%(message)s",
+        level=logging.DEBUG,
     )
+    print(f"Starting websocket ....")
+    websocket = await websockets.connect(HOME_ASSISTANT_URL)
+    print(f"Websocket started!")
+
+    response = await websocket.recv()
+    await handle_message(response)
+    print(f"response1: {response}")
+    await subscribe_to_state(websocket)
+    await listen_for_message()
+
+
+async def listen_for_message():
+    # while True:
+    response = await websocket.recv()
+    print(f"response2: {response}")
+
+
+async def send_data(data):
+    global websocket
+    print(f"Send data: {data}")
+    if websocket:
+        print(f"Send data to websocket: {data}")
+        await websocket.send(data)
+    else:
+        print(f"websocket not initializade")
+
+
+async def handle_message(message):
+    print(f"message: {message}")
+    jsonData = json.loads(message)
+    messageType = jsonData.get(MESSAGE_TYPE)
+    print(f"messageType: {messageType}")
+    if messageType == "auth_required":
+        await auth(websocket, message)
--- senseapp/sense.py
+++ senseapp/sense.py
@@ -1,1 +1,21 @@
+from homeassistant import hassApi
+import asyncio
 
+loop = asyncio.get_event_loop()
+
+
+async def listening_user_input():
+    print(f"Listening for input")
+    while True:
+        user_input = input("Wpisz coś\n")
+        loop.create_task(hassApi.send_data(user_input))
+
+
+def setup():
+
+    loop.create_task(hassApi.start_websocket())
+    # loop.run_until_complete(listening_user_input())
+    loop.run_forever()
+
+
+setup()
