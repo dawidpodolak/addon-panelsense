97cc9f237e9c075373f2658d95cd1b2b4273f9bc
Dawid Podolak <dawidpod1@gmail.com>
2023-08-30 14:37:05 +0000

    Done sense server setup

--- senseapp/sense.py
+++ senseapp/sense.py
@@ -1,4 +1,5 @@
-from homeassistant import hassApi
+from homeassistant import hass_ws_client
+from server import sense_server
 import asyncio
 
 loop = asyncio.get_event_loop()
@@ -8,14 +9,10 @@ async def listening_user_input():
     print(f"Listening for input")
     while True:
         user_input = input("Wpisz coś\n")
-        loop.create_task(hassApi.send_data(user_input))
+        loop.create_task(hass_ws_client.send_data(user_input))
 
-
-def setup():
-
-    loop.create_task(hassApi.start_websocket())
+if __name__ == "__main__":
+    loop.create_task(hass_ws_client.start_haws_client())
+    loop.create_task(sense_server.start_sense_server())
     # loop.run_until_complete(listening_user_input())
     loop.run_forever()
-
-
-setup()
