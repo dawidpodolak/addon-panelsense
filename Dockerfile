ef533fb17de4c1d522947314feb90f1d35033b66
Dawid Podolak <dawidpod1@gmail.com>
2023-10-02 18:20:46 +0000

    Add config options

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 ARG BUILD_FROM
-# FROM $BUILD_FROM
-FROM ghcr.io/home-assistant/amd64-base:3.15
+FROM $BUILD_FROM
+# FROM ghcr.io/home-assistant/amd64-base:3.15
 WORKDIR /usr/src/app
 
 # Copy in the root filesystem for s6-overlay
@@ -17,12 +17,8 @@ COPY senseapp /usr/src/app/senseapp
 COPY requirements.txt /tmp/requirements.txt
 COPY config.yaml /
 ENV SENSE_APP=/usr/src/app/senseapp/sense.py
+ENV HASS_WS_ADDRESS="ws://supervisor/core/websocket"
 
 # Install python dependencies
 RUN pip3 install --upgrade pip
 RUN pip3 install --no-cache-dir -r /tmp/requirements.txt
-
-# ENTRYPOINT ["/init"]
-
-# # Copy data for add-on
-# CMD ["python3", "senseapp/sense.py"]
--- senseapp/sense.py
+++ senseapp/sense.py
@@ -1,11 +1,12 @@
 import asyncio
 import sys
-
+import os
 from homeassistant.components.event_observer import EventObserver
 from homeassistant.home_assistant_client import HomeAssistantClient
 from mediator.mediator import Mediator
 from server.model.server_credentials import ServerCredentials
 from server.sense_server import PanelSenseServer
+from loging.logger import _LOGGER
 
 loop = asyncio.get_event_loop()
 mediator: Mediator
@@ -24,12 +25,24 @@ async def listening_user_input():
     while True:
         user_input = await get_steam_reader(sys.stdin)
 
+def get_server_credentails() -> ServerCredentials:
+    server_credentials = ServerCredentials("admin", "admin")
+    env_server_user = os.getenv("OPTIONS_SERVER_USER")
+    env_server_password = os.getenv("OPTIONS_SERVER_PASSWORD")
+
+    if env_server_user:
+        server_credentials.username = env_server_user
+
+    if env_server_password:
+        server_credentials.password = env_server_password
+
+    _LOGGER.debug(f"Credentials: user: {server_credentials.username}, pswd: {server_credentials.password}")
+    return server_credentials
 
 def main():
     ha_event_observer = EventObserver()
     ha_client = HomeAssistantClient(loop, ha_event_observer)
-    server_credentials = ServerCredentials("admin", "admin")
-    panel_sense_server = PanelSenseServer(loop, server_credentials)
+    panel_sense_server = PanelSenseServer(loop, get_server_credentails())
     mediator = Mediator(ha_client, panel_sense_server)
     # loop.create_task(listening_user_input())
     loop.run_forever()
