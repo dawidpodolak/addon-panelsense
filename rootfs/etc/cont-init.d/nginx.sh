c0e520364b94ad31754d75bb5e84b112f2697ab6
Dawid Podolak <dawidpod1@gmail.com>
2023-10-14 17:37:42 +0000

    Update nginx

--- rootfs/etc/cont-init.d/nginx.sh
+++ rootfs/etc/cont-init.d/nginx.sh
@@ -2,11 +2,11 @@
 # ==============================================================================
 # Configure NGINX
 # ==============================================================================
-ingress_port=$(bashio::addon.ingress_port)
-ingress_entry=$(bashio::addon.ingress_entry)
-ingress_interface=$(bashio::addon.ip_address)
-dns_host=$(bashio::dns.host)
+# ingress_port=$(bashio::addon.ingress_port)
+# ingress_entry=$(bashio::addon.ingress_entry)
+# ingress_interface=$(bashio::addon.ip_address)
+# dns_host=$(bashio::dns.host)
 
 # Retrieve the ingress_entry query path so that nginx can perform rewrites accordingly
-sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/nginx.conf
-sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/nginx.conf
+# sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/nginx.conf
+# sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/nginx.conf
