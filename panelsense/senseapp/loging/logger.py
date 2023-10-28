06cdbb7d1904b9746f27f7847fe3ec27047c7b7a
Dawid Podolak <dawidpod1@gmail.com>
2023-10-28 10:52:02 +0000

    Remove logger

--- panelsense/senseapp/loging/logger.py
+++ panelsense/senseapp/loging/logger.py
@@ -1,9 +1,3 @@
 import logging
-import sys
 
-from loguru import logger
-
-# logger.add(sys.stdout, level="DEBUG")
-
-# logging.basicConfig(level=logging.DEBUG)
 _LOGGER = logging.getLogger(__name__)
