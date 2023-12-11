d84795bc641d9c9eaeb32b28f5ace5a55ef394ed
Dawid Podolak <dawidpod1@gmail.com>
2023-12-11 11:23:30 +0100

    [ISSUE-20] Add installation id #20 (#21)

---
+++ panelsense/senseapp/storage/sense_storage.py
@@ -0,0 +1,16 @@
+import os
+import shelve
+import uuid
+
+databasePath = os.getenv("STORAGE_DATABASE")
+INSTALLATION_ID = "installation_id"
+
+
+def get_installation_id():
+    with shelve.open(databasePath) as storage:
+        installation_id = storage.get(INSTALLATION_ID)
+        if installation_id is None:
+            installation_id = uuid.uuid4()
+            storage[INSTALLATION_ID] = installation_id
+
+        return installation_id
